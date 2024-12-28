32e82cb4d01340c9c0bb330fe6ad56f44d67c23e
Ben Zhang <ben.zhang@watonomous.ca>
2024-12-28 21:38:59 +0000

    Add environment test

---
+++ tests/integration/environment/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Integration test: compression
+# This test aims to verify that the unpacker can handle compressed images
+
+set -o errexit -o nounset -o pipefail
+
+trap 'echo "Error on line $LINENO: $BASH_COMMAND"; exit 1' ERR
+
+docker pull alpine
+
+__tmpdir=$(mktemp -d)
+docker save alpine | APP_LOG_LEVEL=INFO pdm run docker-unpack unpack - "$__tmpdir"
+
+# Test whether the environment variables are correctly set
+apptainer exec --env MYTEST=someteststring "$__tmpdir" env | grep MYTEST
+
+rm -rf "$__tmpdir"
+echo "PASS"
