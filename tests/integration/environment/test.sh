1b09c1d802fb0b0cf7f41b70e2b411c9d3c64f64
Ben Zhang <ben.zhang@watonomous.ca>
2024-12-28 21:52:09 +0000

    Use alpine 3.19

--- tests/integration/environment/test.sh
+++ tests/integration/environment/test.sh
@@ -7,10 +7,10 @@ set -o errexit -o nounset -o pipefail
 
 trap 'echo "Error on line $LINENO: $BASH_COMMAND"; exit 1' ERR
 
-docker pull alpine
+docker pull alpine:3.19
 
 __tmpdir=$(mktemp -d)
-docker save alpine | APP_LOG_LEVEL=INFO pdm run docker-unpack unpack - "$__tmpdir"
+docker save alpine:3.19 | APP_LOG_LEVEL=INFO pdm run docker-unpack unpack - "$__tmpdir"
 
 # Test whether the environment variables are correctly set
 apptainer exec --compat --env MYTEST=someteststring "$__tmpdir" env | grep MYTEST
